005ef8362a48f4d9cd3a12535496cfe49e1c1409
Lukas Siemon <github@blackflux.com>
2021-03-26 17:09:40 -0700

    amend: added missing linebreak

--- src/util/log.js
+++ src/util/log.js
@@ -6,7 +6,7 @@ module.exports = (config) => (...args) => {
     `${args.length === 1 ? '' : ` [${args[0].toUpperCase()}]`}: `,
     `${args[args.length - 1]}`
   ].join('');
-  fs.appendFileSync(config.logFile, log);
+  fs.appendFileSync(config.logFile, `${log}\n`);
   // eslint-disable-next-line no-console
   console.log(log);
 };
