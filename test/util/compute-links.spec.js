0141b69e5d215a53da143c57110e549713dacc7d
Lukas Siemon <github@blackflux.com>
2021-03-26 17:19:18 -0700

    amend: minor

--- test/util/compute-links.spec.js
+++ test/util/compute-links.spec.js
@@ -4,7 +4,7 @@ const computeLinks = require('../../src/util/compute-links');
 
 describe('Testing compute-links.js', () => {
   it('Testing Simple', () => {
-    expect(computeLinks({ links: { 'x': ['a', 'b', 'c'] } })).to.deep.equal({
+    expect(computeLinks({ links: { x: ['a', 'b', 'c'] } })).to.deep.equal({
       a: new Set(['b', 'c']),
       b: new Set(['a', 'c']),
       c: new Set(['a', 'b'])
@@ -14,8 +14,8 @@ describe('Testing compute-links.js', () => {
   it('Testing Overlap', () => {
     expect(computeLinks({
       links: {
-        'x': ['a', 'b', 'c'],
-        'y': ['c', 'd', 'e']
+        x: ['a', 'b', 'c'],
+        y: ['c', 'd', 'e']
       }
     })).to.deep.equal({
       a: new Set(['b', 'c']),
