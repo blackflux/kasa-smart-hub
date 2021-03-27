74b606439fa3e4c37cbb947726cd1a20ac0b0cd1
Lukas Siemon <github@blackflux.com>
2021-03-26 17:19:35 -0700

    amend: config

--- src/resources/config-schema.js
+++ src/resources/config-schema.js
@@ -3,6 +3,12 @@ const Joi = require('joi-strict');
 const timerSchema = Joi.number().integer().min(60);
 
 module.exports = Joi.object().keys({
+  broadcast: Joi.string().ip(),
+  port: Joi.number().integer().min(0).max(65535),
+  breakoutChildren: Joi.boolean(),
+  discoveryInterval: Joi.number().integer().min(0),
+  discoveryTimeout: Joi.number().integer().min(0),
+  offlineTolerance: Joi.number().integer().min(0),
   defaultTimerDuration: timerSchema,
   logFile: Joi.string(),
   links: Joi.object().pattern(Joi.string(), Joi.array().items(Joi.string())),
