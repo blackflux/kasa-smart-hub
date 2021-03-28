267ee01cf2c2cb03e8faad74e7521988d4947064
Lukas Siemon <github@blackflux.com>
2021-03-28 15:40:42 -0700

    amend: minor

--- test/index.spec.js__fixtures/config.js
+++ test/index.spec.js__fixtures/config.js
@@ -2,6 +2,10 @@ const path = require('path');
 
 module.exports = {
   defaultTimerDuration: 23 * 60 * 60,
+  discoveryConfig: {
+    broadcast: '255.255.255.255',
+    port: 1234
+  },
   logFile: path.join(__dirname, 'kasa-logs.txt'),
   links: {
     'kitchen-lights': [
