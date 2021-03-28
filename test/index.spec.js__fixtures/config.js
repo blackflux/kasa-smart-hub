36e15c2bfe77c566a2073723e80e45ef66e6654d
Lukas Siemon <github@blackflux.com>
2021-03-28 15:55:25 -0700

    amend: moved timer default

--- test/index.spec.js__fixtures/config.js
+++ test/index.spec.js__fixtures/config.js
@@ -1,26 +1,13 @@
 const path = require('path');
 
 module.exports = {
-  defaultTimerDuration: 23 * 60 * 60,
   discoveryConfig: {
     broadcast: '255.255.255.255',
     port: 1234
   },
   logFile: path.join(__dirname, 'kasa-logs.txt'),
-  links: {
-    'kitchen-lights': [
-      'Laundry Light',
-      'Kitchen Island',
-      'Kitchen High'
-    ]
-  },
+  links: {},
   timer: {
-    'Master Bathroom Light': 2 * 60 * 60,
-    'Master Bathroom Fan': 3 * 60 * 60,
-    'Guest Bathroom Light': 60 * 60,
-    'Guest Bathroom Fan': 2 * 60 * 60,
-    'TV Mood Light': 3 * 60 * 60,
-    'Fireplace Bottom': 3 * 60 * 60,
-    'Dresser Light': 10 * 60
+    __default: 12 * 60 * 60
   }
 };
