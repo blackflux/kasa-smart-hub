b71e9972d3f33ad803c7191b53632f25e281c544
Lukas Siemon <github@blackflux.com>
2021-03-28 13:33:53 -0700

    amend: separated discoveryConfig

--- src/resources/config-schema.js
+++ src/resources/config-schema.js
@@ -3,12 +3,16 @@ const Joi = require('joi-strict');
 const timerSchema = Joi.number().integer().min(60);
 
 module.exports = Joi.object().keys({
-  broadcast: Joi.string().ip(),
-  port: Joi.number().integer().min(0).max(65535),
-  breakoutChildren: Joi.boolean(),
-  discoveryInterval: Joi.number().integer().min(0),
-  discoveryTimeout: Joi.number().integer().min(0),
-  offlineTolerance: Joi.number().integer().min(0),
+  onDeviceNew: Joi.function(),
+  discoveryConfig: Joi.object().keys({
+    broadcast: Joi.string().ip().optional(),
+    // eslint-disable-next-line newline-per-chained-call
+    port: Joi.number().integer().min(0).max(65535).optional(),
+    breakoutChildren: Joi.boolean().optional(),
+    discoveryInterval: Joi.number().integer().min(0).optional(),
+    discoveryTimeout: Joi.number().integer().min(0).optional(),
+    offlineTolerance: Joi.number().integer().min(0).optional()
+  }).unknown(true),
   defaultTimerDuration: timerSchema,
   logFile: Joi.string(),
   links: Joi.object().pattern(Joi.string(), Joi.array().items(Joi.string())),
