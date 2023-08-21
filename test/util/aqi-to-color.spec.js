4036b08383e97d556b9b96e373f11e866c1136cd
Lukas Siemon <lukas.siemon@blackflux.com>
2023-08-21 08:28:28 -0700

    fix: added extreme range to aqi

--- test/util/aqi-to-color.spec.js
+++ test/util/aqi-to-color.spec.js
@@ -4,7 +4,8 @@ import aqiToColor from '../../src/util/aqi-to-color.js';
 
 describe('Testing aqi-to-color.js', () => {
   it('Testing exact colors', async () => {
-    expect(aqiToColor(300)).to.equal('#41007e');
+    expect(aqiToColor(800)).to.equal('#002e91');
+    expect(aqiToColor(300)).to.equal('#5e00ff');
     expect(aqiToColor(200)).to.equal('#8f3f97');
     expect(aqiToColor(150)).to.equal('#ff0000');
     expect(aqiToColor(100)).to.equal('#ff7e00');
@@ -13,9 +14,13 @@ describe('Testing aqi-to-color.js', () => {
   });
 
   it('Testing out of bound colors', async () => {
-    expect(aqiToColor(350)).to.equal('#41007e');
-    expect(aqiToColor(301)).to.equal('#41007e');
-    expect(aqiToColor(250)).to.equal('#681f8a');
+    expect(aqiToColor(850)).to.equal('#002e91');
+    expect(aqiToColor(801)).to.equal('#002e91');
+    expect(aqiToColor(750)).to.equal('#09299c');
+    expect(aqiToColor(550)).to.equal('#2f17c8');
+    expect(aqiToColor(350)).to.equal('#5404f4');
+    expect(aqiToColor(301)).to.equal('#5d00fe');
+    expect(aqiToColor(250)).to.equal('#761fcb');
     expect(aqiToColor(175)).to.equal('#c71f4b');
     expect(aqiToColor(125)).to.equal('#ff3f00');
     expect(aqiToColor(75)).to.equal('#ffbe00');
